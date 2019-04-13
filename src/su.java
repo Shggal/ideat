018a2199ef313d98c7019f6126b4c55751805ec1
Vossk <1610938611@qq>
2019-04-13 12:39:25 +0800

    sss

--- src/su.java
+++ src/su.java
@@ -3,7 +3,7 @@ public class su {
         System.out.println("No.1");
         s2();
         s3();
-        System.out.println("F");
+        System.out.println("s");
     }
 
   public static void  s2(){
