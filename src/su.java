cb619db8f859713aa717789ce5eff4f8023f04ca
Vossk <1610938611@qq>
2019-04-13 11:43:11 +0800

    S

--- src/su.java
+++ src/su.java
@@ -1,5 +1,18 @@
 public class su {
     public static void main(String[] args) {
         System.out.println("No.1");
+        s2();
+        s3();
+
+
     }
+
+  public static void  s2(){
+      System.out.println("No.2");
+  }
+
+
+  public static void s3(){
+      System.out.println("No.3");
+  }
 }
