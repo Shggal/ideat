ed9a68695164824fb70d8dcc391589df45648de2
Vossk <1610938611@qq>
2019-04-13 13:11:22 +0800

    sss

--- src/su.java
+++ src/su.java
@@ -3,7 +3,7 @@ public class su {
         System.out.println("No.1");
         s2();
         s3();
-        System.out.println("ss");
+        System.out.println("sss");
     }
 
   public static void  s2(){
